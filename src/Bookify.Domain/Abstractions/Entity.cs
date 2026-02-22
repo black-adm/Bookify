d71148c52c86896574f74ed10915fdbdf49572b6
Matheus Madureira <mattheuscontato17@gmail.com>
2026-02-21 22:15:13 -0300

    fix: user domain events

--- src/Bookify.Domain/Abstractions/Entity.cs
+++ src/Bookify.Domain/Abstractions/Entity.cs
@@ -2,5 +2,13 @@
 
 public abstract class Entity(Guid id)
 {
+    private readonly List<IDomainEvent> _domainEvents = [];
+
     public Guid Id { get; init; } = id;
+
+    public IReadOnlyList<IDomainEvent> GetDomainEvents() => _domainEvents.ToList();
+
+    public void ClearDomainEvents() => _domainEvents.Clear();
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent) => _domainEvents.Add(domainEvent);
 }
